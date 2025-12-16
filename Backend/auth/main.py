f3a500c09805cc68b052cdf29040e98f67e41ec7
Tris860 <sezeranoshimo@gmail.com>
2025-12-16 06:47:20 +0200

    hy

--- Backend/auth/main.py
+++ Backend/auth/main.py
@@ -1,26 +1,20 @@
-import time
-import sys
+class Solution:
+   
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        test=""
+        votes=0
+        tested ={}
+        for i in range(len(strs)):
+            for j in range(i+1,len(strs)):
+                test=strs[i][:j]
+                if(test ==strs[j][len(test)-1:len(test)]):
+                   votes=votes+1
+        tested[strs[i]]=votes
+        print(tested)
+        return max(tested,key=tested.get)
 
-def print_lyrics():
-    lyrics =[
-        "wirira wikiyanduriza umutima",
-        "ninge uzi ahondimo nkwerekeza kdi sinakuyobya",
-        "no mu mwijima nzarasa nkumucyo wizuba",
-        "kugirango ugende ahabona, nkumwana w'umwami uganje",
-        "unshingikirizaho gusa ",
-        "urebe uko nzagusindagiza, nkakugeza muri cyagihungu cya mashimwe",
-        "akakaririmbo ndakagutuye mn kdi bonne chance ibiba byose Imana irabizi"
-
-    ]
-    delays = [0.7,0.2,0.5,0.5,1.1,0.6,0.6]
-    print("ninge ubivuze: \n")
-    time.sleep(1.0)
-
-    for i, line in enumerate(lyrics):
-        for char in line:
-            sys.stdout.write(char)
-            sys.stdout.flush()
-            time.sleep(0.05)
-        print()
-    
-        time.sleep(delays[i])
+v = Solution()
+print(v.maxArea([1,8,6,2,5,4,8,3,7]))
+arr = [1,2,3,4,5]
+print(arr[::-1])
+print(v.longestCommonPrefix(["flower","flow","flight"]))
